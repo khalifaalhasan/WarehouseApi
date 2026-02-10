c8146a1d4b71a87b655447f4b43cdee80158fd1e
Khalifa Alhasan <khlfaalhsn5@gmail.com>
2026-02-11 02:08:32 +0700

    feat(app): setup global validation pipe and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [], 
+  imports: [
+    // Load .env file secara global (biar ga perlu import di setiap module)
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+  ],
   controllers: [],
   providers: [],
 })
